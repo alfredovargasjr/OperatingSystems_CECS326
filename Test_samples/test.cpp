5028c185149c44eccf2b9b00c29d086513232750
Alfredo Vargas <vargas.alfredojr@gmail.com>
2018-01-28 14:48:12 -0800

    Add project to repo

---
+++ Test_samples/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "testHeader.h"
+using namespace std;
+
+int main()
+{
+  cout<<"hello world!!\n";
+  Tester t;
+  t.printH();
+  return 0;
+}
---
+++ Test_samples/testHeader.h
@@ -0,0 +1,14 @@
+#ifndef Tester_h
+#define Tester_h
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Tester{
+public:
+  void printH(){
+    cout << "Print from header!!";
+  }
+
+};
+#endif
