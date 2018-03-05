f1ce7f1427c12ac5c8afb5d7500225e08cb07274
Alfredo Vargas <vargas.alfredojr@gmail.com>
2018-03-05 14:17:12 -0800

    Added more annotation

--- Assignment2_InterprocessCommunication/use_msgQ.cpp
+++ Assignment2_InterprocessCommunication/use_msgQ.cpp
@@ -17,11 +17,24 @@ struct buf {
         char greeting[50]; // message content
 };
 
+/*
+-create the first child process
+-use system calls to write and read messages from message queue
+*/
 void child_proc_one(int qid) {
         buf msg;
         int size = sizeof(msg)-sizeof(long);
-        //remove message from the qid queue and place in buffer (msg)
-        //  msgtyp = 0, read first message in the queue
+        
+        /*
+        system call: msgrcv(int msqid, void *msgp, size_t msgsz, long msgtyp, int msgflg);
+                -msqid: id of the message queue created by the OS
+                -*msgp: pointer to the defined structure, (buf buffer)
+                -msgsz: size of msgp message
+                -msgtyp: message type of message in queue
+                -msgflg: message flag behavior when message length is bigger than msgsz
+        -remove message from the qid queue and place in buffer (msg)
+        -msgtyp = 0, read first message in the queue
+        */
         msgrcv(qid, (struct msgbuf *)&msg, size, 113, 0);                                    
         cout << getpid() << ": gets message" << endl;
         //print greeeting from buffer (msg)
@@ -30,15 +43,23 @@ void child_proc_one(int qid) {
         strcat(msg.greeting, " and Adios.");
         cout << getpid() << ": sends reply" << endl;
         msg.mtype = 114;         // prepare message with type mtype = 114
-        //add copy of message from buffer (msg) to the message queue qid
+        
+        /*
+        system call: msgsnd(int msqid, const void *msgp, size_t msgsz, int msgflg)
+                -msqid: id of the message queue created by the OS
+                -*msgp: pointer to the defined structure, (buf buffer)
+                -msgsz: size of msgp message
+                -msgflg: message flag behavior when message length is bigger than msgsz
+        -add copy of message from buffer (msg) to the message queue qid
+        */
         msgsnd(qid, (struct msgbuf *)&msg, size, 0);
-        cout << getpid() << ": now exits" << endl;
+        cout << getpid() << ": now exits" << endl;      
 }
 
 /*
-    -Create a second child process
-    
-    */
+-Create a second child process
+-use system call to write and read messages from message queue
+ */
 void child_proc_two(int qid) {
         buf msg;
         int size = sizeof(msg)-sizeof(long);
@@ -67,7 +88,7 @@ void child_proc_two(int qid) {
 //Message Queue on Linux
 int main() {
 
-        //acquire a message queue from the operating system, message queue ID
+        //acquire a message queue from the operating system, message queue id
         int qid = msgget(IPC_PRIVATE, IPC_EXCL|IPC_CREAT|0600);
 
         //spawning two child processes using the fork() system call
